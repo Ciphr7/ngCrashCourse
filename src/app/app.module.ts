587c32b4c40d87ef0d749ee6814711bef111b7ac
ciphr7 <gilgarcia35@gmail.com>
2020-09-25 16:01:29 -0500

    latest changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,19 @@ import { TodosComponent } from './components/todos/todos.component';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { AngularFireModule } from '@angular/fire';
 
+// 2. Add your credentials from step 1
+const firebaseConfig = {
+  apiKey: "AIzaSyB2lyBCJ2xtQOMht6eeG_IctFM8MqpnrIo",
+  authDomain: "todong-bca0a.firebaseapp.com",
+  databaseURL: "https://todong-bca0a.firebaseio.com",
+  projectId: "todong-bca0a",
+  storageBucket: "todong-bca0a.appspot.com",
+  messagingSenderId: "220409417657",
+  appId: "1:220409417657:web:50c7b7b1611d420e1af068",
+  measurementId: "G-1QZWE4T00H"
+};
 
 @NgModule({
   declarations: [
@@ -18,14 +30,16 @@ import { AddTodoComponent } from './components/add-todo/add-todo.component';
     TodoItemComponent,
     HeaderComponent,
     AddTodoComponent,
-  
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CommonModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+
 
   ],
   providers: [],
